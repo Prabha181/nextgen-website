732205f6b2e9e696c9f87394dfae717f73fcc6d1
Nexera <nexera@gmail.com>
2025-07-28 10:10:32 +0530

    Initial commit for nextgen full-stack app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <GoogleOAuthProvider clientId="930996233111-da7n2q3k4fnc2s2dp8aqd3gjghud2pqo.apps.googleusercontent.com">
     <App />
